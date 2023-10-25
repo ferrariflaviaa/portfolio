a08e3c4e978481c13913569558040b50e8491993
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-10-25 10:54:16 -0300

    feat: specifying each skill

--- src/utils/typeSkill.ts
+++ src/utils/typeSkill.ts
@@ -1,3 +1,5 @@
+import management from "../assets/icon/management.png";
+import nuvem from "../assets/icon/nuvem.png";
 import phone from "../assets/icon/phone.png";
 import sql from "../assets/icon/sql.png";
 import web from "../assets/icon/web.png";
@@ -7,25 +9,31 @@ const typeSkill = [
     id: 1,
     title: "Web Development",
     icon: web,
-    path: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e ",
+    path: "Ampla experiência no desenvolvimento de sistemas, com destaque em ReactJS e tecnologias complementares. Especializado na criação de sistemas eficazes, da concepção à implementação, entregando soluções de alta qualidade sob medida para cada projeto.",
   },
   {
     id: 2,
     title: "App Development",
     icon: phone,
-    path: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e ",
+    path: "Experiência significativa no desenvolvimento de aplicativos, com ênfase em React Native como tecnologia principal. Especializado na criação de aplicativos eficazes, da concepção à implementação, entregando soluções de alta qualidade sob medida para cada projeto. ",
   },
   {
     id: 3,
     title: "SQL Server",
     icon: sql,
-    path: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e ",
+    path: "Experiência sólida em bancos de dados, com foco nas tecnologias MySQL e SQLite. Especializado na gestão e otimização de bases de dados, garantindo eficiência e desempenho para diversas aplicações. ",
   },
   {
     id: 4,
-    title: "SQL Server",
-    icon: sql,
-    path: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e ",
+    title: "API",
+    icon: nuvem,
+    path: "Desenvolvimento de APIs com Node.js, capaz de criar soluções eficazes para diversas necessidades. ",
+  },
+  {
+    id: 4,
+    title: "Gestão de Projetos",
+    icon: management,
+    path: "Experiência em liderança e gestão de projetos, assegurando a entrega de resultados excepcionais por meio de organização e coordenação eficazes.",
   },
 ];
 
